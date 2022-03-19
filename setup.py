24f0891f2d3fe199e1cd564b9b7b9a5ba96309c1
Tomáš Režňák <xreznak@fi.muni.cz>
2022-03-20 00:33:22 +0100

    Add required files for pip

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rat",
+    version="0.0.1",
+    author="Sneklingame",
+    author_email="sneklingame@gmail.com",
+    description="A simple RAndom Testing framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sneklingame/RAT",
+    project_urls={
+        "Bug Tracker": "https://github.com/sneklingame/RAT/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
